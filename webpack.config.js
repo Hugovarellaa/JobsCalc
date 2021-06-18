721298f480e15e421ec477ae02945b8ce3d896d4
Hugo Alves Varella <71407198+Hugovarellaa@users.noreply.github.com>
2021-06-18 19:19:26 -0300

    Configurando Ambiente
    Concluindo modulo 1 Reactjs 11 aulas 01:00:59 Rocketseat

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
-const HtmlWebpackPlugin = require ('html-webpack-plugin');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-    mode: 'development',
-    devtool: 'eval-source-map',
+    mode: isDevelopment ? 'development' : 'production',
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
     entry: path.resolve(__dirname, 'src', 'index.jsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -18,7 +18,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'public' , 'index.html')
+            template: path.resolve(__dirname, 'public', 'index.html')
         })
     ],
     module: {
@@ -27,6 +27,11 @@ module.exports = {
                 test: /\.jsx$/,
                 exclude: /node_modules/,
                 use: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
     }
