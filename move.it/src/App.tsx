c1420de688c2ef0afe3015f5e089f69588691d95
Hugo Alves Varella <71407198+Hugovarellaa@users.noreply.github.com>
2021-07-12 21:34:38 -0300

    Primeira Aula
    Completa

---
+++ move.it/src/App.tsx
@@ -0,0 +1,10 @@
+import { ExperenceBar } from "./components/ExperenceBar";
+import "./styles/global.css";
+
+export function App() {
+  return (
+    <div className="container">
+      <ExperenceBar />
+    </div>
+  );
+}
