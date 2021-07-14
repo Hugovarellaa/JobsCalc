6102051e835b2cc239161f594317e474abbf60c9
Hugo Alves Varella <71407198+Hugovarellaa@users.noreply.github.com>
2021-07-14 10:29:31 -0300

    Segunda Aula
    01:10:00

--- src/components/CounteDown.tsx
+++ src/components/CounteDown.tsx
@@ -1,23 +1,51 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "../styles/components/CounteDown.module.css";
 
 export function CounteDown() {
-  const [] = useState();
+  const [time, setTime] = useState(25 * 60);
+  const [active ,setActive] = useState(false); 
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  const [minuteLeft, minuteRight] = String(minutes)
+   .padStart(2, "0")
+    .split("");
+  const [secondsLeft, secondsRight] = String(seconds)
+    .padStart(2, "0")
+    .split("");
+
+  function startCountDown() {
+    setActive(true);
+  }
+
+  useEffect(()=>{
+    if (active && time > 0){
+      setTimeout(()=>{
+        setTime(time -1)
+      },1000)
+    }
+  } , [active, time])
+
   return (
     <div>
-    <div className={styles.counteDownContainer}>
-      <div>
-        <span>2</span>
-        <span>5</span>
+      <div className={styles.counteDownContainer}>
+        <div>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
+        </div>
+        <span>:</span>
+        <div>
+          <span>{secondsLeft}</span>
+          <span>{secondsRight}</span>
+        </div>
       </div>
-      <span>:</span>
-      <div>
-        <span>0</span>
-        <span>0</span>
-      </div>
-    </div>
 
-      <button type="button" className={styles.countDownButton} >
+      <button
+        type="button"
+        className={styles.countDownButton}
+        onClick={startCountDown}
+      >
         Iniciar um ciclo
       </button>
     </div>
