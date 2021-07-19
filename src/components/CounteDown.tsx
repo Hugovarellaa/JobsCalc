ae9533c1ef132e53236aa525fada988b98990ecd
Hugo Alves Varella <71407198+Hugovarellaa@users.noreply.github.com>
2021-07-19 20:56:52 -0300

    .
    .

--- src/components/CounteDown.tsx
+++ src/components/CounteDown.tsx
@@ -1,44 +1,15 @@
-import { useState, useEffect, useContext } from "react";
-import { ChallengesContext } from "../contexts/ChallengesContexts";
+import { useEffect } from "react";
 import styles from "../styles/components/CounteDown.module.css";
 
-let countcouwnTimeout: NodeJS.Timeout;
+
 
 export function CounteDown() {
-  const { startNewChallege } = useContext(ChallengesContext);
-
-  const [time, setTime] = useState(0.1 * 60);
-  const [isActive, setIsActive] = useState(false);
-  const [hasFinished, setHasFinished] = useState(false);
-
-  const minutes = Math.floor(time / 60);
-  const seconds = time % 60;
-
+  
   const [minuteLeft, minuteRight] = String(minutes).padStart(2, "0").split("");
   const [secondsLeft, secondsRight] = String(seconds)
     .padStart(2, "0")
     .split("");
 
-  function startCountDown() {
-    setIsActive(true);
-  }
-  function resetCountDown() {
-    clearTimeout(countcouwnTimeout);
-    setIsActive(false);
-    setTime(0.1 * 60);
-  }
-
-  useEffect(() => {
-    if (isActive && time > 0) {
-      countcouwnTimeout = setTimeout(() => {
-        setTime(time - 1);
-      }, 1000);
-    } else if (isActive && time === 0) {
-      setHasFinished(true);
-      setIsActive(false);
-      startNewChallege();
-    }
-  }, [isActive, time]);
 
   return (
     <div>
