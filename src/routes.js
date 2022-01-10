74e1cd552df69e62434e555ca71d01033cdca6ea
Hugo <hugovarellaa@gmail.com>
2022-01-10 15:14:56 -0300

    Configuração do servidor Ejs Rodando

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,8 @@
 const express = require("express");
 const routes = express.Router();
-const path = require("path");
-
-const baseURL = path.resolve(__dirname, "views");
-
-routes.get("/", (req, res) => res.sendFile(baseURL + "/index.html"));
-routes.get("/job", (req, res) => res.sendFile(baseURL + "/job.html"));
-routes.get("/job/edit", (req, res) => res.sendFile(baseURL + "/job-edit.html"));
-routes.get("/profile", (req, res) => res.sendFile(baseURL + "/profile.html"));
+routes.get("/", (req, res) => res.render("index"));
+routes.get("/job", (req, res) => res.render("job"));
+routes.get("/job/edit", (req, res) => res.render("job-edit"));
+routes.get("/profile", (req, res) => res.render("profile"));
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const routes = require("./routes");
-
+const path = require("path");
 const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.use(express.static("public"));
 app.use(routes);
 
