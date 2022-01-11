5b91f6ab2e2cbe265cf576445be4d5816e26db15
Hugo <hugovarellaa@gmail.com>
2022-01-10 22:38:58 -0300

    Primeiro pause 01:00:00

--- src/routes.js
+++ src/routes.js
@@ -3,16 +3,29 @@ const routes = express.Router();
 
 const profile = {
   name: "Hugo Alves Varella",
-  avatar: "https://avatars.githubusercontent.com/u/71407198?v=4",
+  avatar: "https://github.com/Hugovarellaa.png",
   "monthly-budget": 3000,
   "hours-per-day": 5,
   "days-per-week": 6,
   "vacation-per-year": 4,
 };
 
+const jobs = [];
+
 routes.get("/", (req, res) => res.render("index"));
+
 routes.get("/job", (req, res) => res.render("job"));
+
+routes.post("/job", (req, res) => {
+  const job = req.body;
+  const date = (job.createAt = Date.now());
+
+  jobs.push(job);
+  return res.redirect("/");
+});
+
 routes.get("/job/edit", (req, res) => res.render("job-edit"));
+
 routes.get("/profile", (req, res) => res.render("profile", { profile }));
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const routes = require("./routes");
 const path = require("path");
+
 const app = express();
+
+app.use(express.urlencoded());
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
